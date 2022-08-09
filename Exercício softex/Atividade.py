382a337b49386fe623e16cbca828564f93b57ed4
artur henrique <arturr.sanntos@gmail.com>
2022-08-09 10:27:02 -0300

    Modificado

--- Exercício softex/Atividade.py
+++ Exercício softex/Atividade.py
@@ -1,43 +1,68 @@
-def calculadora():
- from time import sleep
- n1 = int(input('Digite o primeiro valor: '))
- n2 = int(input('Digite o segundo valor: '))
+import sys
+def adicao(a, b):
+  return (n1 + n2)
 
- opção = 1
+def subtracao(a, b):
+  return (n1 - n2)
 
- while opção != 0: 
+def multiplicacao(a, b):
+  return (n1 * n2)
+
+def divisao(a, b):
+  return (n1 / n2)
+
+def cabecalho():
+    print('-'*25)
+    print('CALCULADORA BÁSICA')
+    print('.'*25)
+    print('''    1. Soma
+    2. Substração
+    3. Multiplicacão
+    4. Divisão
+    ©. Sair''')
+    print('.' *25)
+x = 1
+while x != 0:
+   print(' ')
+   cabecalho()
+   
+   x = int (input ("\nEscolha uma das opções citadas acima: "))
+   if x == 1:
+        print("\nopção escolhida => SOMA: ")
+   elif x == 2:
+        print ("\nopção escolhida =› SUBTRAÇÃO: ")
+   elif x == 3:
+        print ("\nopção escolhida =› MULTIPLICAÇÃO: ")
+   elif x == 4:
+        print ("\nopção escolhida =› DIVISÃO: ")
+   elif x > 4:
+        print(' ')
+        print("\nOpção invalida: ")
+        print('')
+        cabecalho()
+        x = int (input ("\nEscolha uma das opções citadas acima: "))
+   else: 
+        X = 0
+        print("\nProcesso encerrado. \n")
+        exit(0)
+        
+   n1 = float(input("\ninsirao primeiro numero: \n") )
+   n2 = float(input("\ninsira o segundo numero: \n"))
+   
+   
+   if x == 1:
+     print(' ')
+     print ('\n\nResultado da soma entre ', '(',str(n1), ') e (', str(n2),')', ' é igual a (', float(adicao(n1, n2)),') ')
     
-    print('''    {1} somar
-    {2} subtrair
-    {3} multiplicação
-    {4} divisão
-    {5} novos números
-    {0} sair ''')
-
-    opção= int(input('Qual é a sua opção? '))
-    if opção == 1:
-         soma = n1 + n2
-         print('A soma entre {} e {} é {}'.format(n1, n2, soma))
-    elif opção == 2:
-        Subtração = n1 - n2
-        print('A Subtração de {} e {} é {}'.format(n1, n2, Subtração))
-    elif opção == 3:
-        multiplicação = n1 * n2
-        print(' A multiplicação entre {} e {} é {}'.format(n1, n2, multiplicação))   
-    elif opção == 4:   
-        divisão = n1 / n2
-        print('A divisão entre {} e {} é {}'.format(n1, n2, divisão)) 
-    elif opção == 5:
-        print('Informe os números novamente: ') 
-        n1 = int(input('Digite o primeiro valor: '))
-        n2 = int(input('Digite o segundo valor: '))
-         
-    elif opção == 0:
-        print('Finalizando...')
-    else:
-        print('Opção inválida. tente novamente ') 
-    print('=-=' * 10)
-    sleep(2)     
- print('Fim do programa! volte sempre! ')
- 
-calculadora()
+   elif x == 2:
+     print(' ')
+     print('\n\nResultado da subtração entre ', '(',str(n1), ') e (', str(n2),')' , ' é igual a ( ', float(subtracao(n1, n2)),') ')
+       
+   elif x == 3:
+     print(' ')
+     print('\n\nResultado da multiplicação entre ', ' (',str(n1), ') e (', str(n2),')' , ' é igual a (', float(multiplicacao(n1, n2)),') ')
+   elif x == 4:
+     print(' ')
+     print('\n\nResultado da divisão entre ', '(',str(n1), ') e ', str(n2),')' , ' é igual a (', float(divisao(n1, n2)),') ')
+     
+       
